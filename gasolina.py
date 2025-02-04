5d2a2d07bd1dc46934a6ef3539cc293f86d65e15
kaddufernandes <kaddufernandes@gmail.com>
2025-02-04 20:51:24 +0000

    Gera gráfico de linha do preço da gasolina

--- gasolina.py
+++ gasolina.py
@@ -2,9 +2,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# código de geração do gráfico
+# Carrega o arquivo gasolina.csv
+gasolina_df = pd.read_csv('gasolina.csv')
 
-df = pd.read_csv('gasolina.csv')
-
-sns.lineplot(x = 'dia', y = 'venda', data = df)
+# Cria o gráfico de linha
+plt.figure(figsize=(12, 6))
+sns.lineplot(x='dia', y='venda', data=gasolina_df)
+plt.title('Preço Médio da Gasolina em São Paulo (Julho 2021)')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
 plt.savefig('gasolina.png')
+plt.show()
